b9d4f78c1bbeed77413328f4ddbeb65031189af6
Dörner Virag <mal.dorner.virag@gmail.com>
2020-03-31 21:59:32 +0200

    footer kezdete, csatlakozas a projekthez

--- solarboat-app/src/app/app-routing.module.ts
+++ solarboat-app/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { MainpageComponent } from "./mainpage/mainpage.component";
 import { TeamComponent } from "./team/team.component";
+import { FooterComponent } from "./footer/footer.component";
 
 const routes: Routes = [
   { path: "team", component: TeamComponent },
-  { path: "", component: MainpageComponent }
+  { path: "", component: MainpageComponent },
+  { path: "footer", component: FooterComponent }
 ];
 
 @NgModule({
--- solarboat-app/src/app/app.module.ts
+++ solarboat-app/src/app/app.module.ts
@@ -2,15 +2,16 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { FormsModule, NgForm } from "@angular/forms";
 import { HttpClientModule } from "@angular/common/http";
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { MainpageComponent } from "./mainpage/mainpage.component";
 import { TeamComponent } from "./team/team.component";
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, MainpageComponent, TeamComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
+  declarations: [AppComponent, MainpageComponent, TeamComponent, FooterComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, NgbModule],
   providers: [],
   bootstrap: [AppComponent]
 })
