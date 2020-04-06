3ef4b17f63d743e7c2862a8f24759d3b2b626730
Dörner Virag <mal.dorner.virag@gmail.com>
2020-04-06 13:08:23 +0200

    footer, header kb kész, rólunk laptopon kész,
    Footerből a  tooltip hianyzik, nyilvan megint azzal a ***** tooltippel kell b*szakodni. Header ha nagyon letekersz teljesen eltűnik, rólunk ha összecsukod csúnya

--- solarboat-app/src/app/app-routing.module.ts
+++ solarboat-app/src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { Routes, RouterModule } from "@angular/router";
 import { MainpageComponent } from "./mainpage/mainpage.component";
 import { TeamComponent } from "./team/team.component";
 import { FooterComponent } from "./footer/footer.component";
-
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 const routes: Routes = [
   { path: "team", component: TeamComponent },
   { path: "", component: MainpageComponent },
-  { path: "footer", component: FooterComponent }
+  { path: "footer", component: FooterComponent },
+  { path: "nav-bar", component: NavBarComponent }
 ];
 
 @NgModule({
--- solarboat-app/src/app/app.module.ts
+++ solarboat-app/src/app/app.module.ts
@@ -1,18 +1,26 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, NgForm } from "@angular/forms";
-import { HttpClientModule } from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
-import { MainpageComponent } from "./mainpage/mainpage.component";
-import { TeamComponent } from "./team/team.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainpageComponent } from './mainpage/mainpage.component';
+import { TeamComponent } from './team/team.component';
 import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { AboutusComponent } from './aboutus/aboutus.component';
+import { MainpageNewsComponent } from './mainpage-news/mainpage-news.component';
 
 @NgModule({
-  declarations: [AppComponent, MainpageComponent, TeamComponent, FooterComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, NgbModule],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent, MainpageComponent, TeamComponent, FooterComponent, NavBarComponent, AboutusComponent, MainpageNewsComponent],
+  // tslint:disable-next-line:max-line-length
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, NgbModule, MatTooltipModule, MatIconModule, BrowserAnimationsModule, MDBBootstrapModule ],
+  bootstrap: [AppComponent],
+  providers: []
 })
 export class AppModule {}
