51302b7da2449c41ead2b3fee20886401651f8d6
Dörner Virag <mal.dorner.virag@gmail.com>
2020-04-07 13:09:04 +0200

    borító váza kész

--- solarboat-app/src/app/app.module.ts
+++ solarboat-app/src/app/app.module.ts
@@ -16,9 +16,10 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { MainpageNewsComponent } from './mainpage-news/mainpage-news.component';
 import { NewsPreviewComponent } from './news-preview/news-preview.component';
+import { CoverComponent } from './cover/cover.component';
 
 @NgModule({
-  declarations: [AppComponent, MainpageComponent, TeamComponent, FooterComponent, NavBarComponent, AboutusComponent, MainpageNewsComponent, NewsPreviewComponent],
+  declarations: [AppComponent, MainpageComponent, TeamComponent, FooterComponent, NavBarComponent, AboutusComponent, MainpageNewsComponent, NewsPreviewComponent, CoverComponent],
   // tslint:disable-next-line:max-line-length
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, NgbModule, MatTooltipModule, MatIconModule, BrowserAnimationsModule, MDBBootstrapModule ],
   bootstrap: [AppComponent],
