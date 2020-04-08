cf8c18d70238ce0b60283f44818527b275abd908
Dörner Virag <mal.dorner.virag@gmail.com>
2020-04-08 10:57:06 +0200

    hatha most jo lesz

---
+++ solarboat-app/src/app/model/news-data.ts
@@ -0,0 +1,20 @@
+export const news = [
+    {
+        title_hu: 'magyarcim',
+        content_hu: 'magyartartalom',
+        title_en: 'angolcim',
+        content_en: 'angoltartalom',
+        picture: 'itt van hozzá a kép:)',
+        id: 1,
+        date: 2020
+    },
+    {
+        title_hu: 'magyarcimxxxxx',
+        content_hu: 'magyartartalomxxxx',
+        title_en: 'angolcimxxxx',
+        content_en: 'angoltartalomxxxx',
+        picture: 'itt van hozzá a kép:)xxxxx',
+        id: 2,
+        date: 2020
+    }
+];
---
+++ solarboat-app/src/app/news/news.component.ts
@@ -0,0 +1,83 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {News} from '../model/news';
+import {ApiService} from '../shared/api.service';
+import { news } from '../model/news-data';
+
+@Component({
+  selector: 'app-news',
+  templateUrl: './news.component.html',
+  styleUrls: ['./news.component.css']
+})
+export class NewsComponent implements OnInit {
+    allnews: News[] = [];
+    // allnews = news;
+
+  constructor(
+      private http: HttpClient,
+      private apiService: ApiService) {}
+  ngOnInit(): void {
+      this.getNews();
+  }
+
+/*  getNews(): void {
+    console.log('getnews');
+    const json = this.http
+        .get('http://localhost:8080/api/news')
+        .subscribe(data => {
+          console.log(data);
+          this.allnews = data;
+        });
+  }*/
+    public getNews() {
+        this.apiService.getNews().subscribe(
+            res => {
+                this.allnews = res;
+            },
+            err => {
+                alert('get error');
+            }
+        );
+    }
+  postNews(): void {
+      // tslint:disable-next-line:ban-types
+    const o: Object = {
+        title_hu: 'magyarcim',
+        content_hu: 'magyartartalom',
+        title_en: 'angolcim',
+        content_en: 'angoltartalom',
+        picture: 'kepkepkep'
+    };
+    const b = this.http
+        .post('http://localhost:8080/api/news', o)
+        .subscribe(data => {
+          console.log(data);
+        });
+  }
+  putNews(): void {
+    const o: Object = {
+      id: 2,
+      title: 'ez a címe:(',
+      content: 'ez a tartalma:(',
+      picture: 'itt van hozzá a kép:('
+    };
+    const b = this.http
+        .put('http://localhost:8080/api/news', o)
+        .subscribe(data => {
+          console.log(data);
+        });
+  }
+  deleteNews(): void {
+    const b = this.http
+        .delete('http://localhost:8080/api/news/3')
+        .subscribe(data => {
+          console.log(data);
+        });
+  }
+
+  handleClick(event: Event) {
+    console.log('Click!’', event);
+  }
+
+
+}
