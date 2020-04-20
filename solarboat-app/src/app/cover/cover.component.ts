3d787586aa1412604817c298579952723306375c
Dörner Virag <mal.dorner.virag@gmail.com>
2020-04-20 09:08:10 +0200

    solar power data on cover
    saved in sessionstorage, recalculated every 10 minutes

--- solarboat-app/src/app/cover/cover.component.ts
+++ solarboat-app/src/app/cover/cover.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ScrollToService } from 'ng2-scroll-to-el';
 import {HttpClient} from '@angular/common/http';
+import {Observable, Subscription} from 'rxjs';
+import 'rxjs/add/observable/interval';
 
 @Component({
   selector: 'app-cover',
@@ -9,22 +11,43 @@ import {HttpClient} from '@angular/common/http';
 })
 export class CoverComponent implements OnInit {
 
+  public  sub: Subscription;
   public watts;
   constructor(private scrollService: ScrollToService,  private http: HttpClient) { }
 
   ngOnInit(): void {
     this.getWatts();
+    this.sub = Observable.interval(6000000)
+        .subscribe((val) => {
+          const storageData = sessionStorage.getItem('solarpower');
+          if (storageData != null) {
+            const data = JSON.parse(storageData);
+            this.watts = this.setWatts(data);
+          }
+        });
   }
 
+
+
   public getWatts() {
-    this.http
-        .get('https://api.forecast.solar/estimate/watts/47.475498098/19.05333312/0/0/0.312')
-        .subscribe((data) => {
-          this.watts = 0; // number
-          this.watts = this.setWatts(data);
-          console.log('service ' + this.watts);
-          return this.watts;
-        });
+
+    const storageData = sessionStorage.getItem('solarpower');
+    if (storageData === null) {
+      this.http
+          .get('https://api.forecast.solar/estimate/watts/47.475498098/19.05333312/0/0/0.312')
+          .subscribe((data) => {
+            //console.log("using GET");
+            this.watts = this.setWatts(data);
+            sessionStorage.setItem('solarpower', JSON.stringify(data));
+            // console.log('service ' + this.watts);
+            return this.watts;
+          });
+    } else {
+      //console.log("using SessionStorage");
+      const data = JSON.parse(sessionStorage.getItem('solarpower'));
+      this.watts = this.setWatts(data);
+      return this.watts;
+    }
   }
 
 
@@ -36,15 +59,15 @@ export class CoverComponent implements OnInit {
     // dátum + óra: "2020-04-14 05"
     const currentDateHours = date.getFullYear() + '-' + addZero(month) + '-' + addZero(date.getDate()) + ' ' + addZero(date.getHours());
    // const currentDateHours = '2020-04-15 10';
-    console.log(currentDateHours);
+    //console.log(currentDateHours);
 
     // tslint:disable-next-line:only-arrow-functions
     Object.keys(rawData.result).forEach(function(element) {
-      console.log(element, rawData.result[element]);
+      //console.log(element, rawData.result[element]);
 
       if ( element.substring(0, 13) == currentDateHours.toString()) {
-        console.log(element);
-        console.log('if 1 ' + rawData.result[element]);
+       // console.log(element);
+        //console.log('if 1 ' + rawData.result[element]);
         watts = rawData.result[element];
       }
     });
@@ -56,5 +79,4 @@ export class CoverComponent implements OnInit {
     }
     return watts;
   }
-
 }
