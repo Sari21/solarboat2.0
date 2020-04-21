491eab74740ad9ea5fe182b9732a2a923cf5a263
Sinkó Sára <sara@sinko.hu>
2020-04-21 16:04:36 +0200

    pageable news - frontend

--- solarboat-app/src/app/mainpage-news/mainpage-news.component.ts
+++ solarboat-app/src/app/mainpage-news/mainpage-news.component.ts
@@ -1,30 +1,28 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {News} from '../model/news';
-import {ApiService} from '../shared/api.service';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { News } from "../model/news";
+import { ApiService } from "../shared/api.service";
 
 @Component({
-  selector: 'app-mainpage-news',
-  templateUrl: './mainpage-news.component.html',
-  styleUrls: ['./mainpage-news.component.css']
+  selector: "app-mainpage-news",
+  templateUrl: "./mainpage-news.component.html",
+  styleUrls: ["./mainpage-news.component.css"],
 })
 export class MainpageNewsComponent implements OnInit {
-    allnews: News[] = [];
-    constructor(
-        private http: HttpClient,
-        private apiService: ApiService) {
-    }
-    ngOnInit(): void {
-        this.getNews();
-    }
-    public getNews() {
-        this.apiService.getNews().subscribe(
-            res => {
-                this.allnews = res;
-            },
-            err => {
-                alert('get error');
-            }
-        );
-    }
+  allnews: News[] = [];
+  constructor(private http: HttpClient, private apiService: ApiService) {}
+  ngOnInit(): void {
+    this.getNews();
+  }
+  public getNews() {
+    this.apiService.getMainpageNews().subscribe(
+      (res) => {
+        var news: any = res;
+        this.allnews = news.content;
+      },
+      (err) => {
+        alert("get error");
+      }
+    );
+  }
 }
--- solarboat-app/src/app/news/news.component.ts
+++ solarboat-app/src/app/news/news.component.ts
@@ -1,20 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {News} from '../model/news';
-import {ApiService} from '../shared/api.service';
+import { Component, OnInit } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { News } from "../model/news";
+import { ApiService } from "../shared/api.service";
 
 @Component({
-  selector: 'app-news',
-  templateUrl: './news.component.html',
-  styleUrls: ['./news.component.css']
+  selector: "app-news",
+  templateUrl: "./news.component.html",
+  styleUrls: ["./news.component.css"],
 })
 export class NewsComponent implements OnInit {
   allnews: News[] = [];
+  pageNumber: number = 0;
+  isLastPage: boolean = false;
   // allnews = news;
 
-  constructor(
-      private http: HttpClient,
-      private apiService: ApiService) {}
+  constructor(private http: HttpClient, private apiService: ApiService) {}
   ngOnInit(): void {
     this.getNews();
   }
@@ -29,54 +29,58 @@ export class NewsComponent implements OnInit {
           });
     }*/
   public getNews() {
-    this.apiService.getNews().subscribe(
-        res => {
-          this.allnews = res;
-        },
-        err => {
-          alert('get error');
-        }
+    this.apiService.getNews(this.pageNumber).subscribe(
+      (res) => {
+        var data: any = res;
+        <News[]>data.content.forEach((element) => {
+          this.allnews.push(element);
+        });
+        this.pageNumber++;
+        this.isLastPage = data.last;
+      },
+      (err) => {
+        alert("get error");
+      }
     );
   }
+
   postNews(): void {
     // tslint:disable-next-line:ban-types
     const o: Object = {
-      title_hu: 'magyarcim',
-      content_hu: 'magyartartalom',
-      title_en: 'angolcim',
-      content_en: 'angoltartalom',
-      picture: 'kepkepkep'
+      title_hu: "magyarcim",
+      content_hu: "magyartartalom",
+      title_en: "angolcim",
+      content_en: "angoltartalom",
+      picture: "kepkepkep",
     };
     const b = this.http
-        .post('http://localhost:8080/api/news', o)
-        .subscribe(data => {
-          console.log(data);
-        });
+      .post("http://localhost:8080/api/news", o)
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
   putNews(): void {
     const o: Object = {
       id: 2,
-      title: 'ez a címe:(',
-      content: 'ez a tartalma:(',
-      picture: 'itt van hozzá a kép:('
+      title: "ez a címe:(",
+      content: "ez a tartalma:(",
+      picture: "itt van hozzá a kép:(",
     };
     const b = this.http
-        .put('http://localhost:8080/api/news', o)
-        .subscribe(data => {
-          console.log(data);
-        });
+      .put("http://localhost:8080/api/news", o)
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
   deleteNews(): void {
     const b = this.http
-        .delete('http://localhost:8080/api/news/3')
-        .subscribe(data => {
-          console.log(data);
-        });
+      .delete("http://localhost:8080/api/news/3")
+      .subscribe((data) => {
+        console.log(data);
+      });
   }
 
   handleClick(event: Event) {
-    console.log('Click!’', event);
+    console.log("Click!’", event);
   }
-
-
 }
