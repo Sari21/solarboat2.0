8589bbf38d9eb2b71cd003a21fda06206efc6853
Sinkó Sára <sara@sinko.hu>
2020-06-28 00:02:00 +0200

    félkész csapatunk

--- solarboat-app/src/app/shared/team.service.ts
+++ solarboat-app/src/app/shared/team.service.ts
@@ -5,15 +5,16 @@ import { News } from "../model/news";
 import { Achievement } from "../model/achievement";
 import { GalleryPicture } from "../model/gallery-picture";
 import {Globals} from '../globals';
+import * as team from '../../assets/team/team.json'
 
 @Injectable({
   providedIn: "root",
 })
 export class TeamService {
   private BASE_URL = this.globals.BASE_URL + "/api";
-  constructor(private http: HttpClient, public globals: Globals) {}
-
+  constructor(private http: HttpClient, public globals: Globals ) {}
   getTeams() {
-    return this.http.get(this.BASE_URL + '/teams');
+    //return this.http.get(this.BASE_URL + '/teams');
+    return  team;
   }
 }
